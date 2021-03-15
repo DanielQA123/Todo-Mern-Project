8079a80e5c05f8d80e0a5c8ea6a93f8f1aca3c78
DanielQA123 <DAhuchogu@QA.com>
2021-03-15 14:40:06 +0000

    Added gitignore and built my backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+
+//instantiate my app
+const app = express();
+
+
+//Requests 
+app.get("/", (req, res) =>{
+    res.send("hello world!");
+})
+
+app.get("/hello", (req, res) =>{
+    res.send("This is my route");
+})
+
+
+//communicate with the app on a specific port 
+app.listen(5019);
+
+
 console.log("Hey Lets get started");
