b6a015a4bc05a204a880f1275d38134d7b659e23
DanielQA123 <DAhuchogu@QA.com>
2021-03-15 17:05:26 +0000

    Homepage basic page

---
+++ frontend/src/Components/Homepage/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+
+    return (
+        <>
+            <div>
+                <h2> Welcome!! </h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Excepturi rerum accusamus magnam eligendi quia veritatis animi provident cupiditate maxime.
+                Voluptatibus reiciendis dicta officiis, incidunt quae natus necessitatibus doloremque cupiditate delectus.</p>
+            </div>
+        </>
+    )
+
+}
+
+export default Home;
