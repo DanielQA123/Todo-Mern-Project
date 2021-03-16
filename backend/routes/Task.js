104ad6596038cd07bae4361dc1285f829b302e8c
DanielQA123 <DAhuchogu@QA.com>
2021-03-16 14:39:42 +0000

    DA: Making chnages like info

--- backend/routes/Task.js
+++ backend/routes/Task.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const { task } = require("../config/db");
 
-//Request => C.R.U.D
+//All the Request => C.R.U.D
 
 //?Get All(Read All)
 router.get("/getAll", (req, res, next) => {
