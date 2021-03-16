b2ba3263248e8a4ad4c6709ddb40c78cc253db80
DanielQA123 <DAhuchogu@QA.com>
2021-03-16 16:19:41 +0000

    Creating the Navigation bar

--- frontend/src/Components/NavBar/Navbar.jsx
+++ frontend/src/Components/NavBar/Navbar.jsx
@@ -1,12 +1,15 @@
-const Navbar = () => {
+import {Navbar, Nav} from 'react-bootstrap';
+
+const NavbarPage = () => {
     return (
         <>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Dan's Reminders</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#features">Todo's</Nav.Link>
+                    <Nav.Link href="#pricing">Scheduled</Nav.Link>
+                    <Nav.Link href="#pricing">Accomplishments</Nav.Link>
                 </Nav>
             </Navbar>
         </>
@@ -14,4 +17,4 @@ const Navbar = () => {
 
 }
 
-export default Navbar;
+export default NavbarPage;
