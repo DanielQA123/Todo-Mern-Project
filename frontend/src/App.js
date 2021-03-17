24cabae97339d7220600d9f184ed5c080cb0b5cd
DanielQA123 <DAhuchogu@QA.com>
2021-03-17 10:54:38 +0000

    Changed package structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import '../src/CSS/App.css';
 import Home from './Components/Homepage/Home';
+import Page from './Components/SideBar/StandardPage';
+
+
 
 const App = () =>  {
   
   return (
+    
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <Page/>
+    
     </div>
   );
 }
