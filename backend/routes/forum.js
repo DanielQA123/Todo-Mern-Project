2687fd12aa7e57b42c27db0e528c6f529e574d67
DanielQA123 <DAhuchogu@QA.com>
2021-04-06 22:00:44 +0100

    DA: Created the post and get request

--- backend/routes/forum.js
+++ backend/routes/forum.js
@@ -12,3 +12,39 @@ profanity.setOptions({
 
 //! API request => C.R.U.D
 
+//?GET REQUEST:
+router.get("/getAll", (req, res, next) =>{
+    forum.find((err, forum) =>{
+        if(err) {next(err) } else{ res.send(forum)}
+    });
+});
+
+router.get("/get/:id", (req, res, next) =>{
+    forum.findById(req.params.id, (err, result) =>{
+        if(err){
+            next(err);
+        }
+        res.status(200).send(result);
+    });
+});
+
+
+//?POST REQUEST:
+router.post("/create", profanity.init, (req, res, next) =>{
+    const Message = new forum(req.body);
+    Message,save().then((forum) =>{
+        res.status(201).send(`${forum.username} this post has been successfully added!`)
+    })
+    .catch((err) => next(err));
+});
+
+
+//?DELETE REQUEST:
+router.delete("/delete/:id", (req, res, next) =>{
+    
+})
+
+
+//?PATCH REQUEST:
+//?PUT REQUEST:
+
