35f0fd17c2f4ca562a6ca48331e1f243ec402af8
DanielQA123 <DAhuchogu@QA.com>
2021-04-07 16:26:48 +0100

    DA: made changes

--- frontend/src/Components/ForumPage/PrimaryForum.jsx
+++ frontend/src/Components/ForumPage/PrimaryForum.jsx
@@ -1,5 +1,19 @@
+import { Card, CardBody, CardTitle } from "reactstrap"
+import EditForum from "../ForumTabs/EditForum";
+import DeleteForum from "../ForumTabs/DeleteForum";
 
-const PrimaryForum = () =>{
+const PrimaryForum = ({ item, trigger }) => {
+    return (
+        <Card>
+            <CardBody>
+                <CardTitle>{item.username}</CardTitle>
+                <div className="float-right">
+                    <DeleteForum del={item._id} trigger={trigger} />
+                    <EditForum item={item} trigger={trigger}/>
+                </div>
+            </CardBody>
+        </Card>
+    )
 
 }
 
